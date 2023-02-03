e00d5d7740e3a10c4cf6d738c4e25e7d229c40bc
big-dragon-dragon <847617227@qq.com>
2023-02-03 23:31:58 +0800

    0203

---
+++ src/main/java/com/example/demo/MyProjectOfGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectOfGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectOfGitApplication.class, args);
+    }
+
+}
